f4abcc8b248ed6bbe1f61a3e003a8b2986869286
dabbler0606 <dabbler0606@gmail.com>
2021-07-01 06:41:25 +0800

    Initial Commit

---
+++ Shared/LegoalsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegoalsDemoApp.swift
+//  Shared
+//
+//  Created by 王伟豪 on 2021/7/1.
+//
+
+import SwiftUI
+
+@main
+struct LegoalsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
